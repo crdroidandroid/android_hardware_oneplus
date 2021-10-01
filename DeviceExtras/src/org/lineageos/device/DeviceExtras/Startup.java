2586d1179c50a5922a143da376c2790818eb9e6a
AnierinB <anierin@evolution-x.org>
2021-09-30 19:51:04 -0700

    hardware: Initial DeviceExtras 6.0 import!
    - Now using a better resource based configuration method to allow
      universally any OnePlus device to ship this package and configure it
      by using overlays in their device specific tree.
    
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: oddlyspaced <oddlyspaced1@gmail.com>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: jhenrique09 <jhsv09@gmail.com>
    Co-authored-by: AliHasan7671 <AHB7671@gmail.com>
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-bt: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Co-authored-by: SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
    Co-authored-by: Michele Bono <bono.michele94@gmail.com>
    Co-authored-by: LibXZR <xzr467706992@163.com>
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: firebird11 <hbgassel@gmail.com>
    
    DeviceExtras: Use our already available IOnePlusCameraProvider
    
    - No need to import the same thing twice when it's already here.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Change-Id: Ib866f0facf049b647eee6164a46baa0fb546956d

---
+++ DeviceExtras/src/org/lineageos/device/DeviceExtras/Startup.java
@@ -0,0 +1,119 @@
+/*
+* Copyright (C) 2013 The OmniROM Project
+* Copyright (C) 2021-2022 The Evolution X Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.device.DeviceExtras;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.PreferenceManager;
+
+public class Startup extends BroadcastReceiver {
+
+    private static final String TAG = "BootReceiver";
+    private static final String ONE_TIME_TUNABLE_RESTORE = "hardware_tunable_restored";
+
+    private boolean mHBM = false;
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+
+        DeviceExtras.restoreSliderStates(context);
+        EarGainPreference.restore(context);
+        org.lineageos.device.DeviceExtras.doze.DozeUtils.checkDozeService(context);
+        MicGainPreference.restore(context);
+        org.lineageos.device.DeviceExtras.TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
+        VibratorCallStrengthPreference.restore(context);
+        VibratorNotifStrengthPreference.restore(context);
+        VibratorStrengthPreference.restore(context);
+
+        boolean enabled = false;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_DC_SWITCH, false);
+        if (enabled) {
+        restore(DCModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_DCI_SWITCH, false);
+        if (enabled) {
+        mHBM = false;
+        restore(DCIModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_HBM_SWITCH, false);
+        if (enabled) {
+        restore(HBMModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_FPS_INFO, false);
+        if (enabled) {
+            context.startService(new Intent(context, FPSInfoService.class));
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_GAME_SWITCH, false);
+        if (enabled) {
+            restore(GameModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_SRGB_SWITCH, false);
+        if (enabled) {
+        mHBM = false;
+        restore(SRGBModeSwitch.getFile(context), enabled);
+ 	       }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_TOUCH_BOOST_SWITCH, false);
+        if (enabled) {
+        restore(TouchboostModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_TP_EDGE_LIMIT_SWITCH, false);
+        if (enabled) {
+            restore(TPEdgeLimitModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_USB2_SWITCH, false);
+        if (enabled) {
+        restore(USB2FastChargeModeSwitch.getFile(context), enabled);
+               }
+        enabled = sharedPrefs.getBoolean(DeviceExtras.KEY_WIDE_SWITCH, false);
+        if (enabled) {
+        mHBM = false;
+        restore(WideModeSwitch.getFile(context), enabled);
+        }
+    }
+
+    private boolean hasRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean(ONE_TIME_TUNABLE_RESTORE, false);
+    }
+
+    private void setRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        preferences.edit().putBoolean(ONE_TIME_TUNABLE_RESTORE, true).apply();
+    }
+
+    private void restore(String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        if (enabled) {
+            FileUtils.writeValue(file, "1");
+        }
+    }
+
+    private void restore(String file, String value) {
+        if (file == null) {
+            return;
+        }
+        FileUtils.writeValue(file, value);
+    }
+}
