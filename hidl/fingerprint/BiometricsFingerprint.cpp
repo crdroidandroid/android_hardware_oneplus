a492da7f41dd1a4d2a19ec4f2cdbc714d782cbbc
Terminator-J <terminator_j@hotbutteredmonkey.com>
2024-11-14 23:33:50 -0800

    Revert "hidl: fingerprint: Fix UDFPS enrollment and authentication on V"
    This reverts commit e8dd7dfafecc961a508680ece781bb0e4f5effe8.
    
    Reason for revert: This "eats" vendor codes instead of passing them through to framework.
    That fixes enrollment/scanning with vanilla LineageOS framework commits for UdfpsController.
    
    However with our framework commits for AOD without defined sensor and allowing screen-off UDFPS, enrollment & scanning aren't broken in Android 15.
    But screen-off UDPFS *is* broken if vendor code isn't passed through to framework.
    
    Reverting & testing; fp enrollment & regular keyguard scanning still work, *and* AOD/pulse & screen-off UDFPS work again.

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -359,17 +359,9 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t* msg) {
             int32_t vendorCode = 0;
             FingerprintAcquiredInfo result =
                     VendorAcquiredFilter(msg->data.acquired.acquired_info, &vendorCode);
-            if (result == FingerprintAcquiredInfo::ACQUIRED_VENDOR && vendorCode == 0) {
-                // We don't want to inform frameworks about that.
-                ALOGD("onAcquired: Finger down");
-            } else if (result == FingerprintAcquiredInfo::ACQUIRED_VENDOR && vendorCode == 1) {
-                // We don't want to inform frameworks about that.
-                ALOGD("onAcquired: Finger up");
-            } else {
-                ALOGD("onAcquired(%d)", result);
-                if (!thisPtr->mClientCallback->onAcquired(devId, result, vendorCode).isOk()) {
-                    ALOGE("failed to invoke fingerprint onAcquired callback");
-                }
+            ALOGD("onAcquired(%d)", result);
+            if (!thisPtr->mClientCallback->onAcquired(devId, result, vendorCode).isOk()) {
+                ALOGE("failed to invoke fingerprint onAcquired callback");
             }
         } break;
         case FINGERPRINT_TEMPLATE_ENROLLING:
